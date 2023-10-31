103b84108f5e4fdb08cb070e3a7a2b35fd5d2cf3
Natandso <minatosantana@hotmail.com>
2023-10-31 18:29:32 -0300

    fazendo nossos testes de teams e teams by id

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -3,10 +3,12 @@ import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
+import { teams, team } from './mocks/teams.mock';
 import { app } from '../app';
 import Example from '../database/models/ExampleModel';
 
 import { Response } from 'superagent';
+import SequelizeTeams from '../database/models/TeamsModel';
 
 chai.use(chaiHttp);
 
@@ -39,7 +41,31 @@ describe('Seu teste', () => {
   //   expect(...)
   // });
 
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+  it('shoudl return all teams', async function() {
+sinon.stub(SequelizeTeams, 'findAll').resolves(teams as any)
+
+const { status, body }= await chai.request(app).get('/teams');
+
+expect(status).to.eq(200);
+expect(body).to.deep.eq(team)
   });
+
+  it('shoudl return a team by id', async function() {
+    sinon.stub(SequelizeTeams, 'findOne').resolves(teams as any)
+    
+    const { status, body }= await chai.request(app).get('/teams/1');
+    
+    expect(status).to.eq(200);
+    expect(body).to.deep.eq(team)
+      });
+
+      it('should return not found if the team doesn\'t exists', async function() {
+        sinon.stub(SequelizeTeams, 'findOne').resolves(null);
+    
+        const { status, body } = await chai.request(app).get('/teams/1');
+    
+        expect(status).to.equal(404);
+        expect(body.message).to.equal('team 1 not found');
+      });
+  afterEach(sinon.restore);
 });
